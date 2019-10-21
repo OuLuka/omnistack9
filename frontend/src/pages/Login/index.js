e57707ef15cc48f3d519d58f6e1d33b670f70912
Lucas <lf.gouvea@gmail.com>
2019-10-21 00:55:25 -0300

    Terminando o frontend

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,5 +1,35 @@
-import React from 'react';
+import React, { useState } from "react";
+import api from "../../services/api";
 
-export default function Login() {
-    return <div />
+export default function Login({ history }) {
+    const [email, setEmail] = useState("");
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+        const response = await api.post("/sessions", { email });
+        const { _id } = response.data;
+        localStorage.setItem('user',_id);
+        history.push('/dashboard')
+    }
+    return(
+    <>
+        <p>
+            Ofereça <strong>spots</strong> para programadores e encontre{" "}
+            <strong>talentos</strong> para
+        </p>
+        <form onSubmit={handleSubmit}>
+        <label htmlFor="email">E-mail *</label>
+        <input
+            type="email"
+            id="email"
+            placeholder="Seu email"
+            value={email}
+            onChange={event => setEmail(event.target.value)}
+        />
+        <button className="btn " type="submit">
+            Entrar
+        </button>
+        </form>
+    </>
+    )
 }
