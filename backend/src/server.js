48906ad0ce4bd7b107721c012fc348e9403d49df
Lucas Faria <lfaria.gouvea@gmail.com>
2019-10-28 22:18:13 -0300

    Socket.io Backend e Frontend Web

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-
 const path = require("path");
+const socketio = require("socket.io");
+const http = require("http");
 
 const routes = require("./routes");
 
 const app = express();
+const server = http.Server(app);
+const io = socketio(server);
 
 mongoose.connect(
   "mongodb+srv://ouluka:970318@cluster0-8ywt0.mongodb.net/omnistack9?retryWrites=true&w=majority",
@@ -15,10 +18,23 @@ mongoose.connect(
     useUnifiedTopology: true
   }
 );
+
+const connectedUsers = {};
+io.on("connection", socket => {
+  const { user_id } = socket.handshake.query;
+  connectedUsers[user_id] = socket.id;
+});
+
+app.use((req, res, next) => {
+  req.io = io;
+  req.connectedUsers = connectedUsers;
+  return next();
+});
+
 //app.use(cors({ origin: "http://localhost:3000" }));
 app.use(cors());
 app.use(express.json());
 app.use("/files", express.static(path.resolve(__dirname, "..", "uploads")));
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
